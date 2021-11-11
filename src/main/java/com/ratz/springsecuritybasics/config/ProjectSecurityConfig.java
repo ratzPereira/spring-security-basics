45ae1a449ee99485c59da99ea228135e92795313
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-11-11 13:47:02 -0100

    Added JWT. created jwt generator and validator, added the needed configs to validate the token and created helper class to store constants

--- src/main/java/com/ratz/springsecuritybasics/config/ProjectSecurityConfig.java
+++ src/main/java/com/ratz/springsecuritybasics/config/ProjectSecurityConfig.java
@@ -1,25 +1,35 @@
 package com.ratz.springsecuritybasics.config;
 
+import com.ratz.springsecuritybasics.filter.JWTTokenGeneratorFilter;
+import com.ratz.springsecuritybasics.filter.JWTTokenValidatorFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true,securedEnabled = true,jsr250Enabled = true)
 public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(new CorsConfigurationSource() {
+        //indicates spring security to not generate any tokens
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().
+                cors().configurationSource(new CorsConfigurationSource() {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
@@ -27,15 +37,18 @@ public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setExposedHeaders(Arrays.asList("Authorization"));
                         config.setMaxAge(3600L);
                         return config;
                     }
-                }).and().csrf().disable().authorizeRequests()
-                .and().authorizeRequests()
+                }).and().csrf().disable()
+                .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
+                .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
+                .authorizeRequests()
                 .antMatchers("/myAccount").hasRole("USER")
                 .antMatchers("/myBalance").hasAnyRole("USER","ADMIN")
-                .antMatchers("/myLoans").hasRole("ROOT")
-                .antMatchers("/myCards").authenticated()
+                .antMatchers("/myLoans").authenticated()
+                .antMatchers("/myCards").hasAnyRole("USER","ADMIN")
                 .antMatchers("/user").authenticated()
                 .antMatchers("/notices").permitAll()
                 .antMatchers("/contact").permitAll().and().httpBasic();
--- src/main/java/com/ratz/springsecuritybasics/services/LoanServiceImpl.java
+++ src/main/java/com/ratz/springsecuritybasics/services/LoanServiceImpl.java
@@ -3,6 +3,7 @@ package com.ratz.springsecuritybasics.services;
 import com.ratz.springsecuritybasics.Entity.Loans;
 import com.ratz.springsecuritybasics.repository.LoanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,6 +15,7 @@ public class LoanServiceImpl implements LoanService {
     private LoanRepository repository;
 
     @Override
+    @PreAuthorize("hasRole('ROOT')")
     public List<Loans> findByCustomerIdOrder(int customerId) {
         return repository.findByCustomerIdOrderByStartDtDesc(customerId);
     }
